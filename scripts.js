b5df0c5d5133b061a3d67fcd4939c2b780a625f2
Alan Machado Ferreira <alanmferreira@hotmail.com>
2023-11-08 05:19:06 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,28 +1,58 @@
-function decrementaNumeros(){
-
-    const numero = parseFloat(document.getElementById("number").value);
-
-    // Diminui de 100 até 0 pelo número digitado
-
-    for(let i = 100; i >=0; i=i-numero) {
-        document.getElementById("mensagem").innerHTML ="Verifique no console o resultado!"
-        console.log(i)
+class CamisasDeClubes{
+    constructor(clube, modelo, tamanho, temporada, quantidade){
+        this.clube = clube;
+        this.modelo = modelo;
+        this.tamanho = tamanho;
+        this.temporada = temporada;
+        this.quantidade = quantidade;
     }
+}
+
+function carregarTabela(){
+    const tabela = document.querySelector("#corpoTabela");
+    
+    const corpoTabela =  document.createElement("tr");
+    tabela.appendChild(corpoTabela);
+
+    const linha = document.createElement("th");
+    corpoTabela.appendChild(linha);
+
+    const coluna1 = document.createElement("td");
+    coluna1.classList.add("clube");
+    coluna1.innerText = CamisasDeClubes.clube;
+    corpoTabela.appendChild(coluna1);
+
+    const coluna2 = document.createElement("td");
+    coluna2.classList.add("modelo");     
+    corpoTabela.appendChild(coluna2);
+
+    const coluna3 = document.createElement("td");
+    coluna3.classList.add("temporada");
+    corpoTabela.appendChild(coluna3);
+
+    const coluna4 = document.createElement("td");
+    coluna4.classList.add("tamanho");
+    corpoTabela.appendChild(coluna4);
+
+    const coluna5 = document.createElement("td");
+    coluna5.classList.add("quantidade");
+    corpoTabela.appendChild(coluna5);    
+
+}
+
+function cadastrar(){
+    const clube = document.getElementById("nomeClube").value;
+    const modelo = document.getElementById("modelo").value;
+    const tamanho = document.getElementById("tamanho").value;
+    const temporada = document.getElementById("temporada").value;
+    const quantidade = document.getElementById("quantidade").value
+
+    const camisa1 = new CamisasDeClubes(clube, modelo, tamanho, temporada, quantidade);
+
+    carregarTabela();
 
 };
 
-function valorParcelas(){
 
-    const valor = parseFloat(document.getElementById("valor").value);
-    const parcela = parseFloat(document.getElementById("parcelas").value);
-    var valorParcelas = 0;
-    var i = 1;
 
-    while (i <= parcela) {
-        valorParcelas = valor / parcela;
-        console.log("O Valor da " + i +"º- Parcela é: R$" + valorParcelas +",00");
-        i++;
-        document.getElementById("mensagem").innerHTML ="Verifique no console o resultado!"
-    }
 
-}
